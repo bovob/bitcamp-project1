ecbb1dd429aacb9cb27d7c6a9164e9a282b2e719
bovob <jw_dot@kakao.com>
2024-06-27 16:58:07 +0900

    M3 수정

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -27,7 +27,14 @@ public class Test1 {
   }
 
     static void m3() {
-        System.out.println("M3");
+      String[] arr = new String[]{"a","b","c"};
+
+      System.out.println("M3");
+      for(int i=0; i<arr.length ; i++){
+          System.out.println(arr[i]);
+      }
+        System.out.println("END");
+
     }
 
   static void m9() {
