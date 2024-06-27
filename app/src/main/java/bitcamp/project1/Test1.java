0e1b7ebc74a8de4b7bcc6d5db61c3e3abfcfeb33
uripup <airin00100@gmail.com>
2024-06-27 16:02:27 +0900

    m2

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -2,4 +2,12 @@ package bitcamp.project1;
 
 public class Test1 {
   public int a;
+
+  public static void main(String[] args) {
+    System.out.println("git test");
+  }
+
+  static void m2() {
+    System.out.println("M2");
+  }
 }
