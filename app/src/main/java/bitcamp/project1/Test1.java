dbcb2a208fe8367bbc302b02b09ff6662691be36
bovob <jw_dot@kakao.com>
2024-06-27 15:59:33 +0900

    Test1 변경

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -1,5 +1,15 @@
 package bitcamp.project1;
 
 public class Test1 {
-  public int a;
+
+  void Test1(){
+    System.out.println("git Test 입니다.");
+  }
+
+  public static void main(String[] args) {
+
+    Test1 t = new Test1();
+
+  }
+
 }
